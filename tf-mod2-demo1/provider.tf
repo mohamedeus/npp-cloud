34c2f6ffefa16b68d56bc3055dff1f8ca40420b4
Mohamed Younes <mohamedyounes@Mohameds-MBP.attlocal.net>
2025-04-24 23:42:45 -0700

    mod main and provider

--- tf-mod2-demo1/provider.tf
+++ tf-mod2-demo1/provider.tf
@@ -1,9 +1,9 @@
 
 //https://registry.terraform.io/providers/hashicorp/google/latest/docs/guides/provider_reference
 provider "google" {
-  credentials = file("FILL-IN.json")
+  credentials = file("../../farro-437723-2de645c68dcd.json")
 
-  project = "FILL-IN"
+  project = "farro-437723"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
